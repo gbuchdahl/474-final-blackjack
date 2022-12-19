7d1aa7a5df420837cc6bcbef90a18b88370e3569
treyskid <65044989+treyskid@users.noreply.github.com>
2022-12-19 13:54:35 -0500

    full basic strategy

--- test_blackjack.py
+++ test_blackjack.py
@@ -7,16 +7,69 @@ from q_learning import QStrategyStdBet
 
 class TestStrategy(BlackjackStrategy):
     def select_action(self, hand: PlayableHand, shoe: Shoe) -> Action:
-        if hand.hand[0].value == hand.hand[1].value and hand.hand[0].value == 8:
+        if hand.hand.is_blackjack():
+            return Action.STAND
+        if len(hand.hand) == 2 and hand.hand[0].value == hand.hand[1].value and hand.hand[0].value == 8:
             return Action.SPLIT
-        if hand.get_value()[0] == 11:
+        elif len(hand.hand) == 2 and hand.hand[0].value == hand.hand[1].value and hand.hand[0].value == 1:
+            return Action.SPLIT
+        elif len(hand.hand) == 2 and hand.hand[0].value == hand.hand[1].value and hand.hand[0].value == 1:
+            if hand.upcard.get_value() != 7 and hand.upcard.get_value() != 10 and hand.upcard.get_value() != 1:
+                return Action.SPLIT
+        elif len(hand.hand) == 2 and hand.hand[0].value == hand.hand[1].value and hand.hand[0].value == 7 and hand.upcard.get_value() <= 7:
+            return Action.SPLIT
+        elif len(hand.hand) == 2 and hand.hand[0].value == hand.hand[1].value and hand.hand[0].value == 6 and hand.upcard.get_value() <= 6:
+            return Action.SPLIT
+        elif len(hand.hand) == 2 and hand.hand[0].value == hand.hand[1].value and hand.hand[0].value == 4 and (hand.upcard.get_value() == 5 or hand.upcard.get_value() == 6):
+            return Action.SPLIT
+        elif len(hand.hand) == 2 and hand.hand[0].value == hand.hand[1].value and hand.hand[0].value == 2 and hand.upcard.get_value() <= 7:
+            return Action.SPLIT
+        elif len(hand.hand) == 2 and hand.hand[0].value == hand.hand[1].value and hand.hand[0].value == 3 and hand.upcard.get_value() <= 7:
+            return Action.SPLIT
+
+        elif hand.get_value()[1] and hand.get_value()[0] == 20:
+            return Action.STAND
+        elif hand.get_value()[1] and hand.get_value()[0] == 19:
+            if len(hand.hand) == 2 and hand.upcard.get_value() == 6:
+                return Action.DOUBLE
+            return Action.STAND
+        elif hand.get_value()[1] and hand.get_value()[0] == 18:
+            if hand.upcard.get_value() == 1 or hand.upcard.get_value() == 10 or hand.upcard.get_value() == 9:
+                return Action.HIT
+            elif len(hand.hand) == 2 and hand.upcard.get_value() <= 6:
+                return Action.DOUBLE
+            else:
+                return Action.STAND
+        elif hand.get_value()[1] and (hand.upcard.get_value() <= 2 or hand.upcard.get_value() >= 7):
+            return Action.HIT
+        elif hand.get_value()[1] and hand.upcard.get_value() == 3 and hand.get_value()[0] <= 16:
+            return Action.HIT
+        elif hand.get_value()[1] and hand.upcard.get_value() == 4 and hand.get_value()[0] <= 14:
+            return Action.HIT
+        elif len(hand.hand) == 2 and hand.get_value()[1]:
+            return Action.DOUBLE
+            
+       
+
+
+        elif hand.get_value()[0] == 11:
             return Action.DOUBLE
         elif hand.get_value()[0] == 10 and hand.upcard.get_value() != 10 and hand.upcard.get_value() != 1:
             return Action.DOUBLE
+        elif hand.get_value()[0] == 9 and hand.upcard.get_value() >= 3 and hand.upcard.get_value() <= 6:
+            return Action.DOUBLE
+        
+
+
+
+
         elif hand.get_value()[0] <= 16 and (hand.upcard.get_value() >= 7 or hand.upcard.get_value() == 1):
+            return Action.HIT
+        elif hand.get_value()[0] == 12 and (hand.upcard.get_value() == 2 or hand.upcard.get_value() == 3):
             return Action.HIT
         elif hand.get_value()[0] >= 12 and hand.upcard.get_value() >= 2 and hand.upcard.get_value() <= 6:
             return Action.STAND
+        
         elif hand.get_value()[0] <= 11:
             return Action.HIT
         else:
@@ -27,12 +80,12 @@ class TestStrategy(BlackjackStrategy):
 
 
 
-strategy = QStrategyStdBet()
-Q_dict = strategy.q_learn()
-strategy.print_strategy()
-
-# strategy = TestStrategy()
+# strategy = QStrategyStdBet()
+# Q_dict = strategy.q_learn()
 # strategy.print_strategy()
+
+strategy = TestStrategy()
+strategy.print_strategy()
 
 results = []
 for _ in range(10):
